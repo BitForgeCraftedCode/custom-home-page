d86f7fd152d7f20ebc660797366b021af862d5be
BitForgeCraftedCode <11381086+BitForgeCraftedCode@users.noreply.github.com>
2018-12-28 10:52:49 -0500

    style settings form

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -8,40 +8,48 @@ class Settings extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state={
-			bgChoice: '--Please choose an option--'
+			bgChoice: '--Please choose an option--',
+			delay: '--Please choose an option--'
 		}
 	}
 
-	handleChange(e) {
+	handleBgChange(e) {
 		this.setState({bgChoice: e.target.value});
+	}
+
+	handleDelayChange(e) {
+		this.setState({delay: e.target.value});
 	}
 
 	handleSubmit(e) {
 		e.preventDefault();
-		console.log(this.state.bgChoice);
-		if(this.state.bgChoice === '--Please choose an option--') {
-			alert('Please choose a background category');
+		console.log(this.state.delay);
+		if(this.state.bgChoice === '--Please choose an option--' || this.state.delay === '--Please choose an option--') {
+			alert('Please select background category and delay');
 			return;
 		}
 		this.props.switchBackgrounds(this.state.bgChoice);
+		this.resetForm();
 	}
 
 	resetForm() {
 		this.setState({
-			bgChoice: '--Please choose an option--'
+			bgChoice: '--Please choose an option--',
+			delay: '--Please choose an option--'
 		});
 	}
 
 	render() {
 		return(
-			<form onSubmit={(e) => this.handleSubmit(e)}>
-				<fieldset>
-					<legend>Choose a background category</legend>
-					<label htmlFor="backgrounds-select">Backgrounds:</label>
+			<form className="settings" onSubmit={(e) => this.handleSubmit(e)}>
+				<fieldset className="settings__fieldset">
+					<legend className="settings__legend">Choose a background category:</legend>
+					<label className="settings__label" htmlFor="backgrounds-select">Backgrounds:</label>
 					<select
+						className="settings__select"
 						id="backgrounds-select"
 						value={this.state.bgChoice}
-						onChange={(e) => this.handleChange(e)}
+						onChange={(e) => this.handleBgChange(e)}
 					>
 					    <option value="--Please choose an option--" disabled>--Please choose an option--</option>
 					    <option value="Nature">Nature</option>
@@ -50,22 +58,39 @@ class Settings extends React.Component {
 					    <option value="Animals">Animals</option>
 					    <option value="Ocean">Ocean</option>
 					</select>
-					<div>
-						<button
-							type="submit"
-							value="Submit"
-						>
-							Switch Backgrounds
-						</button>
-						<button
-							type="button"
-							value="Reset"
-							onClick={() => this.resetForm()}
-						>
-							Cancel
-						</button>
-					</div>
 				</fieldset>
+				<fieldset className="settings__fieldset">
+					<legend className="settings__legend">Choose background delay:</legend>
+					<label className="settings__label" htmlFor="delay-select">Delay:</label>
+					<select
+						className="settings__select"
+						id="delay-select"
+						value={this.state.delay}
+						onChange={(e) => this.handleDelayChange(e)}
+					>
+					    <option value="--Please choose an option--" disabled>--Please choose an option--</option>
+					    <option value="5">5 Seconds</option>
+					    <option value="10">10 Seconds</option>
+					    <option value="15">15 Seconds</option>
+					    <option value="20">20 Seconds</option>
+					    <option value="30">30 Seconds</option>
+					</select>
+				</fieldset>
+				<div className="settings__btnContainer">
+					<button
+						type="submit"
+						value="Submit"
+					>
+						Change Settings
+					</button>
+					<button
+						type="button"
+						value="Reset"
+						onClick={() => this.resetForm()}
+					>
+						Cancel
+					</button>
+				</div>
 			</form>
 		);
 	}
