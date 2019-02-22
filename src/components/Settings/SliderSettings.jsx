c6922247d081d65d292bd5cd65429d48341348e6
BitForgeCraftedCode <11381086+BitForgeCraftedCode@users.noreply.github.com>
2019-02-22 09:53:04 -0500

    style slider settings

--- src/components/Settings/SliderSettings.jsx
+++ src/components/Settings/SliderSettings.jsx
@@ -7,23 +7,26 @@ import { switchBackgrounds, changeDelay } from '../../redux/actions';
 class SliderSettings extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state={
+		this.state = {
 			bgChoice: '--Please choose an option--',
 			delay: '--Please choose an option--'
-		}
+		};
 	}
 
 	handleBgChange(e) {
-		this.setState({bgChoice: e.target.value});
+		this.setState({ bgChoice: e.target.value });
 	}
 
 	handleDelayChange(e) {
-		this.setState({delay: e.target.value});
+		this.setState({ delay: e.target.value });
 	}
 
 	handleSubmit(e) {
 		e.preventDefault();
-		if(this.state.bgChoice === '--Please choose an option--' || this.state.delay === '--Please choose an option--') {
+		if (
+			this.state.bgChoice === '--Please choose an option--' ||
+			this.state.delay === '--Please choose an option--'
+		) {
 			alert('Please select background category and delay');
 			return;
 		}
@@ -40,54 +43,59 @@ class SliderSettings extends React.Component {
 	}
 
 	render() {
-		return(
-			<form className="SliderSettings" onSubmit={(e) => this.handleSubmit(e)}>
+		return (
+			<form className="SliderSettings" onSubmit={e => this.handleSubmit(e)}>
 				<fieldset className="SliderSettings__fieldset">
 					<legend className="SliderSettings__legend">Choose Background Category:</legend>
-					<label className="SliderSettings__label" htmlFor="backgrounds-select">Backgrounds:</label>
-					<select
-						className="SliderSettings__select"
-						id="backgrounds-select"
-						value={this.state.bgChoice}
-						onChange={(e) => this.handleBgChange(e)}
-					>
-					    <option value="--Please choose an option--" disabled>--Please choose an option--</option>
-					    <option value="Nature">Nature</option>
-					    <option value="Space">Space</option>
-					    <option value="CitySkylines">CitySkylines</option>
-					    <option value="Animals">Animals</option>
-					    <option value="Ocean">Ocean</option>
-					</select>
+					<label className="SliderSettings__label" htmlFor="backgrounds-select">
+						Backgrounds:
+					</label>
+					<div className="SliderSettings__select-div">
+						<select
+							className="SliderSettings__select"
+							id="backgrounds-select"
+							value={this.state.bgChoice}
+							onChange={e => this.handleBgChange(e)}
+						>
+							<option value="--Please choose an option--" disabled>
+								--Please choose an option--
+							</option>
+							<option value="Nature">Nature</option>
+							<option value="Space">Space</option>
+							<option value="CitySkylines">CitySkylines</option>
+							<option value="Animals">Animals</option>
+							<option value="Ocean">Ocean</option>
+						</select>
+					</div>
 				</fieldset>
 				<fieldset className="SliderSettings__fieldset">
 					<legend className="SliderSettings__legend">Choose Background Delay:</legend>
-					<label className="SliderSettings__label" htmlFor="delay-select">Delay:</label>
-					<select
-						className="SliderSettings__select"
-						id="delay-select"
-						value={this.state.delay}
-						onChange={(e) => this.handleDelayChange(e)}
-					>
-					    <option value="--Please choose an option--" disabled>--Please choose an option--</option>
-					    <option value="5">5 Seconds</option>
-					    <option value="10">10 Seconds</option>
-					    <option value="15">15 Seconds</option>
-					    <option value="20">20 Seconds</option>
-					    <option value="30">30 Seconds</option>
-					</select>
+					<label className="SliderSettings__label" htmlFor="delay-select">
+						Delay:
+					</label>
+					<div className="SliderSettings__select-div">
+						<select
+							className="SliderSettings__select"
+							id="delay-select"
+							value={this.state.delay}
+							onChange={e => this.handleDelayChange(e)}
+						>
+							<option value="--Please choose an option--" disabled>
+								--Please choose an option--
+							</option>
+							<option value="5">5 Seconds</option>
+							<option value="10">10 Seconds</option>
+							<option value="15">15 Seconds</option>
+							<option value="20">20 Seconds</option>
+							<option value="30">30 Seconds</option>
+						</select>
+					</div>
 				</fieldset>
 				<div className="SliderSettings__btnContainer">
-					<button
-						type="submit"
-						value="Submit"
-					>
+					<button type="submit" value="Submit">
 						Change Slideshow Settings
 					</button>
-					<button
-						type="button"
-						value="Reset"
-						onClick={() => this.resetForm()}
-					>
+					<button type="button" value="Reset" onClick={() => this.resetForm()}>
 						Cancel
 					</button>
 				</div>
@@ -101,5 +109,7 @@ const mapDispatchToProps = {
 	changeDelay: changeDelay
 };
 
-export default connect(null, mapDispatchToProps)(SliderSettings);
-
+export default connect(
+	null,
+	mapDispatchToProps
+)(SliderSettings);
